292c1a402d013d761bd23664e968489769520195
tien95vt <ttienn123@gmail.com>
2018-05-02 21:42:55 +0700

    update

---
+++ app/controllers/libraries/index.js
@@ -0,0 +1,25 @@
+import Controller from '@ember/controller';
+import Ember from 'ember';
+
+
+export default Controller.extend({
+
+  //queryParams: ['filter', 'letter'],
+
+  filteredList: Ember.computed('model.@each.name', 'filter', function() {
+
+    let results = this.get('model');
+    const query = this.get('filter');
+    if (query) {
+      // One of the best regular expression website: http://www.regexr.com/
+      // Split the query at spaces and join them to get like this: /(word1)+.*(word2)+.*(word3)+.*/ig
+      const regexString = '(' + query.split(' ').join(')+.*(') + ')+.*';
+      // i: case insensitive, g: global
+      const regex = new RegExp(regexString, 'ig');
+
+      results = results.filter((item) => item.get('name').match(regex));
+    }
+
+    return results.sortBy('name');
+  })
+});
--- app/routes/libraries/index.js
+++ app/routes/libraries/index.js
@@ -1,9 +1,25 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  // queryParams: {
+  //   letter: { refreshModel: true }
+  // },
 
   model() {
     return this.store.findAll('library');
+    // return this.store.query('library', {
+    //   orderBy: 'name'
+    // });
+  },
+
+  actions: {
+    deleteLibrary(library) {
+      let confirmation = confirm('Are you sure?');
+
+      if (confirmation) {
+        library.destroyRecord();
+      }
+    }
   }
 
 });
